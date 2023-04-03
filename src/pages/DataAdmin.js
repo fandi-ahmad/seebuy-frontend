d2ac18f440c59c9313a3dd9b097073178b236701
fandi-ahmad <fandi4160@gmail.com>
2023-04-03 23:51:05 +0800

    test push from ubuntu

--- src/pages/DataAdmin.js
+++ src/pages/DataAdmin.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import { Navbar } from '../components/Navbar'
 import { Pagination } from '../components/Pagination'
-import { Sidebar } from '../components/Sidebar'
 import { Adminpanel } from '../layouts/Adminpanel'
 
 const DataAdmin = () => {
